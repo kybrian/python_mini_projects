009078cb9fcfabb92a1be9377fd03c05aed0237e
kybrian <kybrian.se@gmail.com>
2023-06-07 09:21:26 +0300

    Tip Calculator End

--- 02_tip_calculator.py
+++ 02_tip_calculator.py
@@ -1,15 +1,13 @@
-# Instructions at:
-# https://replit.com/@appbrewery/tip-calculator-start#README.md
-
-print("Welcome to tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? "))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
+tipPercentage = tip / 100
+totalBill = 1 + tipPercentage
 
-print(f"Each person should pay: + ${final_amount}")
+eachPerson = float(bill / people) * totalBill
+
+eachPerson = round(eachPerson, 2)
+
+print(f"Each person should pay: {eachPerson}")
