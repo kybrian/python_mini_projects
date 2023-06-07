2c31d77c8d2b39b0110fcf675dc8b7c98a22e056
kybrian <kybrian.se@gmail.com>
2023-06-07 11:18:20 +0300

    rock paper scissors complete

--- 04_rock_paper_scissors.py
+++ 04_rock_paper_scissors.py
@@ -1,3 +1,8 @@
+# Official Game Website : https://wrpsa.com/the-official-rules-of-rock-paper-scissors/
+
+# Import random module 
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +29,27 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+gameOptions = [rock, paper, scissors]
+
+playerChoice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+if playerChoice >= 3 or playerChoice < 0:
+    print("Invalid number. You lose!")
+else:
+    print(gameOptions[playerChoice])
+print(gameOptions[playerChoice])
+
+computerChoice = random.randint(0,2)
+print("Computer chose: ")
+print(gameOptions[computerChoice])
+    
+if playerChoice == 0 and computerChoice == 2:
+    print("You Win!")
+elif computerChoice == 0 and playerChoice == 2:
+    print("You Lose!")
+elif playerChoice > computerChoice:
+    print("You Win!")
+elif computerChoice > playerChoice:
+    print("You Lose!")
+elif computerChoice == playerChoice:
+    print("You draw!")
