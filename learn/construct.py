96fa37c131b1fa908d48c5d3dda2a0dbc3a5650e
kybrian <kybrian.se@gmail.com>
2023-06-12 12:23:52 +0300

    Hangman final

---
+++ learn/construct.py
@@ -0,0 +1,35 @@
+import sys #used for the sys.exit function 
+
+target_int = input("How many integers?")
+
+try:
+    target_int = int(target_int)
+except ValueError:
+    sys.exit("You must enter an integer!")
+
+ints = list()
+
+count = 0
+
+while count < target_int:
+    new_int = input("Please enter integer{0}:".format(count + 1))
+    isint = False 
+    try:
+        new_int = int(new_int)
+    except:
+        print("You must enter an integer")
+
+    if isint == True:
+        ints.append(new_int)
+        count += 1 
+
+print("Using a for loop")
+for value in ints:
+    print(str(value))
+
+print("Using a while loop")
+total = len(ints)
+count = 0
+while count < total:
+    print(str(ints[count]))
+    count += 1
